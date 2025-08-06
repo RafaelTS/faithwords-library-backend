7f597a4b2fcd58d2ea0976236b7b08a8025011e8
RafaelTS <rafaeltorress@gmail.com>
2025-08-06 15:15:13 -0300

    basic struct

---
+++ src/main/java/org/rafaelts/faithwords_library_backend/FaithwordsLibraryBackendApplication.java
@@ -0,0 +1,13 @@
+package org.rafaelts.faithwords_library_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaithwordsLibraryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaithwordsLibraryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/rafaelts/faithwords_library_backend/FaithwordsLibraryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rafaelts.faithwords_library_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaithwordsLibraryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
