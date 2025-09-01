1bf763c407019919e374f08c702095b15fabeb62
RafaelTS <rafaeltorress@gmail.com>
2025-09-01 11:41:47 -0300

    test

--- src/main/java/org/rafaelts/faithwords_library_backend/domain/model/Book.java
+++ src/main/java/org/rafaelts/faithwords_library_backend/domain/model/Book.java
@@ -2,14 +2,15 @@ package org.rafaelts.faithwords_library_backend.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "books")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class Book {
 
     @Id
@@ -27,7 +28,56 @@ public class Book {
     @Column(name = "quantity")
     private int quantity;
 
+    // Conveniência
     public boolean isAvailable() {
         return quantity > 0;
+    }
+
+    // Builder manual
+    public static BookBuilder builder() {
+        return new BookBuilder();
+    }
+
+    public static class BookBuilder {
+        private Long id;
+        private String title;
+        private String author;
+        private String medium;
+        private boolean forRent;
+        private int quantity;
+
+        public BookBuilder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public BookBuilder title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public BookBuilder author(String author) {
+            this.author = author;
+            return this;
+        }
+
+        public BookBuilder medium(String medium) {
+            this.medium = medium;
+            return this;
+        }
+
+        public BookBuilder forRent(boolean forRent) {
+            this.forRent = forRent;
+            return this;
+        }
+
+        public BookBuilder quantity(int quantity) {
+            this.quantity = quantity;
+            return this;
+        }
+
+        public Book build() {
+            return new Book(id, title, author, medium, forRent, quantity);
+        }
     }
 }
--- src/main/java/org/rafaelts/faithwords_library_backend/domain/model/User.java
+++ src/main/java/org/rafaelts/faithwords_library_backend/domain/model/User.java
@@ -1,20 +1,63 @@
 package org.rafaelts.faithwords_library_backend.domain.model;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String username;
     private String password;
     private String role;
+
+    public User(Long id, String username, String password, String role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    // Builder manual
+    public static UserBuilder builder() {
+        return new UserBuilder();
+    }
+
+    public static class UserBuilder {
+        private Long id;
+        private String username;
+        private String password;
+        private String role;
+
+        public UserBuilder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public UserBuilder username(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public UserBuilder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public UserBuilder role(String role) {
+            this.role = role;
+            return this;
+        }
+
+        public User build() {
+            return new User(id, username, password, role);
+        }
+    }
 }
